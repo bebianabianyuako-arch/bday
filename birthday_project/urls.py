7cf7385a74921639074b9b221ef3abeb4c197cbb
bimanyuw <f.abimanyuwijanarko@gmail.com>
2025-11-26 19:20:02 +0700

    update

--- birthday_project/urls.py
+++ birthday_project/urls.py
@@ -1,8 +1,12 @@
-from django.contrib import admin
+# File: birthday_project/urls.py
+
 from django.urls import path
+# Hapus baris di bawah ini
+# from django.contrib import admin 
 from wishes.views import home
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    # Hapus atau komentari baris di bawah ini
+    # path('admin/', admin.site.urls), 
     path('', home, name='home'),
 ]
